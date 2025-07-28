2ac87244c452afa336142b3010ddad3d8dcf27ba
Vitaly <vsg.sam@gmail.com>
2025-07-28 17:57:43 +0200

    somw reorganization

--- data/loaders.py
+++ data/loaders.py
@@ -2,7 +2,7 @@ import json
 
 
 def load_words(language, difficulty, logger):
-    path_to_file = f"words/{language}_{difficulty}_words.json"
+    path_to_file = f"data/words/{language}_{difficulty}_words.json"
     try:
         with open(path_to_file, 'r', encoding='utf-8') as f:
             return json.load(f)
